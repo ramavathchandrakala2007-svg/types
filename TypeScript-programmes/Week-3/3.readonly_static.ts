a99a407c4b32c81f33852dbfdc8d5b462459fa9c
ramavathchandrakala2007-svg <ramavathchandrakala2007@gmail.com>
2026-08-12 17:43:51 +0530

    Replace Book class with Product class

--- TypeScript-programmes/Week-3/3.readonly_static.ts
+++ TypeScript-programmes/Week-3/3.readonly_static.ts
@@ -1,22 +1,24 @@
-class Book {
-    static libraryName: string = "City Library";
-    readonly bookId: number;
+class Product {
+    static storeName: string = "Smart Store";
+    readonly productId: number;
 
-    title: string;
+    productName: string;
 
-    constructor(id: number, title: string) {
-        this.bookId = id;
-        this.title = title;
+    constructor(id: number, name: string) {
+        this.productId = id;
+        this.productName = name;
     }
 
     display(): void {
-        console.log("Book ID: " + this.bookId);
-        console.log("Book Title: " + this.title);
+        console.log("Product ID: " + this.productId);
+        console.log("Product Name: " + this.productName);
     }
 }
 
-console.log(Book.libraryName);
-const book1 = new Book(101, "Python");
-const book2 = new Book(102, "TypeScript");
-book1.display();
-book2.display();
+console.log(Product.storeName);
+
+const product1 = new Product(201, "Laptop");
+const product2 = new Product(202, "Mobile");
+
+product1.display();
+product2.display();
